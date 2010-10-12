e6a9e8ef97555c006572bc946d100ec73785e5bf
Patrick Roberts <patrick@silentbluesystems.com>
2010-10-12 16:19:01 -0700

    Rails doesn't like a capital in a migration, and also added a label and checkbox

--- redmine_mediawiki/db/migrate/002_add_mediawikiIframe_to_wiki.rb
+++
@@ -1,9 +0,0 @@
-class AddMediawikiToWiki < ActiveRecord::Migration
-  def self.up
-    add_column :wikis, :keep_internal, :string
-  end
-
-  def self.down
-    remove_column :wikis, :keep_internal
-  end
-end
