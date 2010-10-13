1616fd1f7712933a339dce04306c3216294494b6
Patrick Roberts <Patrick@silentbluesystems.com>
2010-10-13 09:28:25 -0700

    Finished the plugin

--- redmine_mediawiki/lib/wiki_controller_patch.rb
+++ redmine_mediawiki/lib/wiki_controller_patch.rb
@@ -4,21 +4,16 @@ module Wikimedia
     module WikiControllerPatch
 
       def index_with_wiki_bypass
-
-      if @wiki.external_wiki_link && @wiki.redirect_wiki
-		if @wiki.keep_internal
-			render 'wiki/iframe'
-		else
-			redirect_to @wiki.external_wiki_link
-		end
-        
-      else
-        #redirect_to "http://yahoo.com"
-        index_without_wiki_bypass
+        if @wiki.external_wiki_link && @wiki.redirect_wiki
+		      if @wiki.keep_internal
+			      render :action => 'iframe'
+		      else
+			        redirect_to @wiki.external_wiki_link
+		      end
+        else
+          index_without_wiki_bypass
+        end
       end
-   end
-      
-
       def self.included(base)
         base.class_eval do
           unloadable
