4fbf52e175dbb46a65af1158202e3ec65305df3f
Patrick Roberts <Patrick@silentbluesystems.com>
2010-10-14 08:44:14 -0700

    iFrame

--- redmine_mediawiki/init.rb
+++ redmine_mediawiki/init.rb
@@ -3,6 +3,6 @@ require_dependency 'wiki_controller_patch.rb'
 Redmine::Plugin.register :redmine_mediawiki do
   name 'Redmine Wikimedia plugin'
   author 'Patrick Roberts'
-  description 'This is a plugin that will redirect the wiki link to an external site'
-  version '0.0.1'
+  description 'This is a plugin that will redirect the wiki link to an external site.  Alternatively lets you use an Iframe instead.'
+  version '0.0.2'
  end
